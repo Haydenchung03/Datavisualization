24d842eee1b6bd4e55729664af6d2aa0fefa690a
Haydenchung03 <Haydenchung21@hotmail.com>
2022-09-30 16:52:59 -0400

    Created model for net profit

--- DataVisualization/DataAnalytics/models.py
+++ DataVisualization/DataAnalytics/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+# Model for net profit
+class netProfit(models.Model):
+
+    # Example net income
+    netIncome = models.FloatField()
+    revenue = models.FloatField()
+    netProfit = (netIncome / revenue) * 100
+    
+    def __str__(self):
+        return self.netProfit
+
+class Item(models.Model):
+    netProfitItem = models.ForeignKey(netProfit, on_delete=models.CASCADE)
+    text = models.CharField(max_length = 300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
