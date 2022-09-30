3fd9ba4517397d02f4925d2eec6f9c15e8493809
Haydenchung03 <Haydenchung21@hotmail.com>
2022-09-30 17:37:27 -0400

    Added net income model

--- DataVisualization/DataAnalytics/models.py
+++ DataVisualization/DataAnalytics/models.py
@@ -9,14 +9,7 @@ class netProfit(models.Model):
     netIncome = models.FloatField()
     revenue = models.FloatField()
     
-    def __str__(self):
+    
+    def __float__(self):
         return (self.netIncome / self.revenue) * 100
-
-class Item(models.Model):
-    netProfitItem = models.ForeignKey(netProfit, on_delete=models.CASCADE)
-    text = models.CharField(max_length = 300)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
 
