78307e30388a1401ba37a15ea3a70cf71f1e8f6d
Haydenchung03 <Haydenchung21@hotmail.com>
2022-10-17 17:32:27 -0400

    Added a pie chart in home page

--- DataVisualization/DataAnalytics/urls.py
+++ DataVisualization/DataAnalytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     # Adding path to url 
     # allow user to search for position in sql database
@@ -9,3 +10,5 @@ urlpatterns = [
     path('login/', views.login, name = "login"),
     path('contact/', views.contact, name = "contact")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DataVisualization/DataAnalytics/views.py
+++ DataVisualization/DataAnalytics/views.py
@@ -4,11 +4,31 @@ from numpy import NaN
 from .models import customerAquisition, netProfit, netProfitMargin
 from django.core.mail import send_mail
 from django.conf import settings
+import plotly
+from plotly.offline import plot
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.graph_objs import *
 
 # Create your views here.
 
 def home(response):
-    return render(response, "DataAnalytics/home.html", {})
+    # This dataframe has 244 lines, but 4 distinct values for `day`
+    
+    df = px.data.tips()
+    labels = ['Businesses that do not have data unused','Businesses that do have data unused']
+    values = [3, 97]
+    config = {'displayModeBar': False}
+    # pull is given as a fraction of the pie radius
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0.1, 0])])
+    fig.update(layout_showlegend = False)
+    fig.update_traces(hoverinfo = 'label+percent', textinfo = 'none')
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'})
+    
+
+    context = fig.to_html(config = config)
+    return render(response, "DataAnalytics/home.html", {'context':context})
 
 def login(request):
     
